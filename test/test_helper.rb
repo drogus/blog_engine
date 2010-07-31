2e15c37a98f225ff6cff0aa379c4606b22db419e
Piotr Sarnacki <drogus@gmail.com>
2010-07-31 14:01:24 +0200

    Ignore *.blog.rb migrations in dummy app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,7 +18,7 @@ Capybara.default_selector = :css
 # Run any available migration
 # A bit of hacks, find a nicer way
 FileUtils.rm(Dir[File.expand_path("../dummy/db/test.sqlite3", __FILE__)])
-FileUtils.rm(Dir[File.expand_path("../dummy/db/migrate/*.rb", __FILE__)])
+FileUtils.rm(Dir[File.expand_path("../dummy/db/migrate/*.blog.rb", __FILE__)])
 ActiveRecord::Migration.copy File.expand_path("../dummy/db/migrate/", __FILE__), { :blog => File.expand_path("../../db/migrate/", __FILE__) }
 ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
 
