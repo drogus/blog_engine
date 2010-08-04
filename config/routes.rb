7901f54227753136e3cf679df8160c4ec5ddcc37
Piotr Sarnacki <drogus@gmail.com>
2010-08-04 23:14:59 +0200

    Currently, the approach to use routes is to avoid prefix from namespaces, to avoid calling something like blog.blog_post_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Blog::Engine.routes.draw do
-  namespace :blog, :path => nil, :shallow_path => nil do
+  namespace :blog, :path => nil, :shallow_path => nil, :as => nil do
     resources :posts, :controller => "posts"
     root :to => "posts#index"
   end
